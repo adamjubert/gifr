bd58428f8ae70d9b800c27ecdf898e7c5385d8f0
Adam Jubert <jubert.adam@gmail.com>
2017-03-12 17:55:04 -0400

    Add README and change styling

---
+++ README.md
@@ -0,0 +1,32 @@
+# gifr
+
+gifr is a single-page web app inspired by Flickr. It utilizes my own DOM manipulation library, [domSawyer](https://github.com/adamjubert/domSawyer), to handle events and send AJAX requests to the GIPHY API.
+
+[Live](https://adamjubert.github.io/gifr)
+
+## Features
+
+Users can add a gif to the page either by selecting the 'Add Dog Gif' button or inputing a specific query in the search bar. This triggers a `domSawyer` AJAX request to the GIPHY API:
+
+```
+function fetchGif(tag) {
+  $ds.ajax({
+    method: 'GET',
+    url: `https://api.giphy.com/v1/gifs/random?api_key=*************&tag=${tag}&rating=g`,
+    success: data => addGifToRow(data)
+  });
+}
+```
+
+Users can remove the last gif and remove all gifs from the screen. There are also three toggle buttons that, when clicked, toggle a specific action on/off:
+* Zoom gifs 20% on hover
+* Add a clickable link to each gif
+* Expand gif size and only display one per row
+
+``` function toggleZoom() {
+  $ds('#action-zoom').toggleClass("selected");
+  $ds('.gif').toggleClass("zoom");
+}
+```
+
+These toggle buttons affect all gifs currently on the screen and remain in effect even after all gifs have been removed.
--- js/gifr.js
+++ js/gifr.js
@@ -6,6 +6,11 @@ function addClasses() {
   return classes;
 }
 
+function addDescription(item) {
+  let text = whichToAdd(item);
+  $ds(".action-description").append(`<p>${text}</p>`);
+}
+
 function addGifToRow(data) {
   let url = JSON.parse(data).data.image_url;
   if (url.split("")[4] !== 's') { // ensure image is https
@@ -13,11 +18,6 @@ function addGifToRow(data) {
   }
   let img = `<img class="${ addClasses() }" src="${ url }" />`;
   $ds('.gif-wrapper').last().append(img);
-}
-
-function addDescription(item) {
-  let text = whichToAdd(item);
-  $ds(".action-description").append(`<p>${text}</p>`);
 }
 
 function addRowOfGifs() {
@@ -36,11 +36,6 @@ function addSingleGif(tag = "dog") {
   fetchGif(tag);
 }
 
-function searchForGif() {
-  let query = $ds("#search-form").elements()[0].value;
-  addSingleGif(query);
-}
-
 function fetchGif(tag) {
   $ds.ajax({
     method: 'GET',
@@ -49,23 +44,8 @@ function fetchGif(tag) {
   });
 }
 
-function whichToAdd(item) {
-  switch (item) {
-    case 'add':
-      return "Sends an AJAX request to the giphy API and appends the received gif to the last child";
-    case 'remove':
-      return "Removes the last '.row' child element from the screen";
-    case 'removeAll':
-      return "Removes all elements with class 'row' (and therefore, all gifs) from the screen";
-    case 'zoom':
-      return "Toggles a class that adds a 'transform: scale(1.2)' property on hover";
-    case 'click':
-      return "Toggles an on-click event handler that opens the image's url in a new window";
-    case 'mobile':
-      return "Toggles a class that adds a 'flex-direction: column' property to each row and increases image width and height ";
-    default:
-      return "";
-  }
+function noGifsOnScreen() {
+  return ( $ds('.gif').length() === 0 );
 }
 
 function removeAllGifs() {
@@ -86,13 +66,16 @@ function removeLastGif() {
   }
 }
 
-function noGifsOnScreen() {
-  return ( $ds('.gif').length() === 0 );
-}
 
 function removeRow() {
   $ds('.gif').last().remove();
 }
+
+function searchForGif() {
+  let query = $ds("#search-form").elements()[0].value;
+  addSingleGif(query);
+}
+
 
 function toggleClickableGifs() {
   $ds('#action-clickable').toggleClass("selected");
@@ -114,4 +97,23 @@ function toggleMobile() {
 function toggleZoom() {
   $ds('#action-zoom').toggleClass("selected");
   $ds('.gif').toggleClass("zoom");
+}
+
+function whichToAdd(item) {
+  switch (item) {
+    case 'add':
+      return "Sends an AJAX request to the giphy API and appends the received gif to the last child";
+    case 'remove':
+      return "Removes the last '.row' child element from the screen";
+    case 'removeAll':
+      return "Removes all elements with class 'row' (and therefore, all gifs) from the screen";
+    case 'zoom':
+      return "Toggles a class that adds a 'transform: scale(1.2)' property on hover";
+    case 'click':
+      return "Toggles an on-click event handler that opens the image's url in a new window";
+    case 'mobile':
+      return "Toggles a class that adds a 'flex-direction: column' property to each row and increases image width and height ";
+    default:
+      return "";
+  }
 }
